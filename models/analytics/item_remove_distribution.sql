7eb48a487237b9d190c35aabfa97a2e50d304dc9
henanbei huang <hhnb042522@163.com>
2024-04-23 19:29:28 +0000

    item remove

---
+++ models/analytics/item_remove_distribution.sql
@@ -0,0 +1,23 @@
+select '40-55',
+sum(case when REMOVE_QUANTITY_IN_LAST_WEEK < 50 then 1 else 0 end) as amount
+from  {{ ref('dim_item') }} 
+UNION ALL
+select '55-70', 
+sum(case when REMOVE_QUANTITY_IN_LAST_WEEK > 55 and REMOVE_QUANTITY_IN_LAST_WEEK < 70 then 1 else 0 end) 
+from 
+{{ ref('dim_item') }}
+UNION ALL
+select '70-85', 
+sum(case when REMOVE_QUANTITY_IN_LAST_WEEK > 70 and REMOVE_QUANTITY_IN_LAST_WEEK < 85 then 1 else 0 end) 
+from 
+{{ ref('dim_item') }}
+UNION ALL
+select '85-100', 
+sum(case when REMOVE_QUANTITY_IN_LAST_WEEK > 85 and REMOVE_QUANTITY_IN_LAST_WEEK < 100 then 1 else 0 end) 
+from 
+{{ ref('dim_item') }}
+UNION ALL
+select '100-115', 
+sum(case when REMOVE_QUANTITY_IN_LAST_WEEK > 100 then 1 else 0 end)
+from 
+{{ ref('dim_item') }}
