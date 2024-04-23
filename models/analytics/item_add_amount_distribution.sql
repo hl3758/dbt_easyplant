b40fc8b9ba65aa0e9cee2b7cb8b27b23851f24be
henanbei huang <hhnb042522@163.com>
2024-04-23 18:37:14 +0000

    item distribution

--- models/analytics/item_add_amount_distribution.sql
+++ models/analytics/item_add_amount_distribution.sql
@@ -1,27 +1,27 @@
-select 'a_first_range',
+select '600-650',
 sum(case when ADD_QUANTITY_IN_LAST_WEEK < 650 then 1 else 0 end) as amount
 from  {{ ref('dim_item') }}
 UNION ALL
-select 'b_second_range', 
+select '650-700', 
 sum(case when ADD_QUANTITY_IN_LAST_WEEK > 650 and ADD_QUANTITY_IN_LAST_WEEK < 700 then 1 else 0 end) 
 from 
 {{ ref('dim_item') }}
 UNION ALL
-select 'c_third_range', 
+select '700-750', 
 sum(case when ADD_QUANTITY_IN_LAST_WEEK > 700 and ADD_QUANTITY_IN_LAST_WEEK < 750 then 1 else 0 end) 
 from 
 {{ ref('dim_item') }}
 UNION ALL
-select 'd_forth_range', 
+select '750-800', 
 sum(case when ADD_QUANTITY_IN_LAST_WEEK > 750 and ADD_QUANTITY_IN_LAST_WEEK < 800 then 1 else 0 end) 
 from 
 {{ ref('dim_item') }}
 UNION ALL
-select 'e_fifth_range', 
+select '850-850', 
 sum(case when ADD_QUANTITY_IN_LAST_WEEK > 800 and ADD_QUANTITY_IN_LAST_WEEK < 850 then 1 else 0 end)
 from 
 {{ ref('dim_item') }}
 UNION ALL
-select 'f_sixth_range', 
+select '850-900', 
 sum(case when ADD_QUANTITY_IN_LAST_WEEK > 850 and ADD_QUANTITY_IN_LAST_WEEK < 900 then 1 else 0 end) 
 from  {{ ref('dim_item') }}
